6e99dd684099569d3e4fa25ea461e2bfed284420
Dominik Biesalski <dominik.biesalski@adesso.de>
2024-11-26 11:24:36 +0100

    Initial Commit

---
+++ src/main/java/com/example/kendocompetitionhelper/KendoCompetitionHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.kendocompetitionhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KendoCompetitionHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KendoCompetitionHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kendocompetitionhelper/KendoCompetitionHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kendocompetitionhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KendoCompetitionHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
